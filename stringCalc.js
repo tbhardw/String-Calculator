14ee191e345d93ae557ba5f4e5b44f58d8812b64
Tanvi Bhardwaj <tanvibhardwaj@Tanvis-MacBook-Pro.local>
2018-06-27 16:49:40 -0700

    Week 3 Kata

--- stringCalc.js
+++ stringCalc.js
@@ -1,57 +1,23 @@
-// function stringCalc(id){
-// 	let string_numbers = id;
-// 	var numbers = string_numbers.replace(/\n|;|\\/g,",").split(',');
-// 	console.log(numbers);
-// 		var sum = 0;
-// 		for(var i = 0; i < numbers.length;i++){
-// 			console.log('entering')
-// 			if(numbers[i] < 0){
-// 				throw new Error('negative numbers are not allowed');
-// 			}
-
-// 			if(isNaN(numbers[i])){
-// 				sum = sum + 0;
-// 			}
-// 			// else if(numbers[i] < 0){
-// 			// 	throw new Error('Negative numbers not allowed');
-// 			// }
-// 			else if(numbers.length === 1){
-// 				sum = parseInt(numbers);
-// 			}
-// 			else{
-// 				sum = sum + parseInt(numbers[i]);
-// 			}
-// 		}
-// 			alert('the sum is '+sum);
-// 			return sum;
-// }
-// module.exports = stringCalc;
-
-
 function stringCalc(id){
 	let string_numbers = id;
-	console.log(string_numbers);
+	//Testing for the negative numbers 
 	if(string_numbers.includes('-')){
 		throw new Error('negative numbers are not allowed');
 	}
-	var numbers = string_numbers.replace(/\n|;|\\/g,",");
+	//replaces the numbers with a comma symbol
+	var numbers = string_numbers.replace(/\n|;|\/|\\/g,",");
 	console.log(numbers);
+	//Splitting by commas 
+	var rem = numbers.split(',');
 	var sum = 0;
-	if(numbers == ''){
-		alert('the sum is '+0);
-		return 0;
-	}
-	else if(numbers.length == 1){
-		alert('the sum is '+parseInt(numbers));
-		return parseInt(numbers);
-	}
-	else{
-		var rem = numbers.split(',');
 		for(var i = 0; i < rem.length;i++){
-			sum = sum + parseInt(rem[i]);
+			//to check if an array element is not an empty string
+			if(rem[i] != ''){
+				sum = sum + parseInt(rem[i]);
+			}
 		}
+		alert('the sum is '+sum);
 		return sum;
 	}
-			alert('the sum is '+sum);
-}
+	
 module.exports = stringCalc;
--- stringCalc.test.js
+++ stringCalc.test.js
@@ -1,26 +1,43 @@
 'use strict';
-//import stringCalc from 'stringCalc.js';
 
 const stringCalc = require('./stringCalc');
-//const stringCalc = require("./stringCalc");
-console.log('looking for the test ', Object.getOwnPropertyNames(stringCalculator));
 
-//test to add two numbers 
-test('Sum of 1,2 is 3', () => {
-    expect(stringCalc(1,2)).toBe(3);
+//Test 1: test to add two numbers 
+test('To Test the sum of two numbers', () => {
+    expect(stringCalc('1,2')).toBe(3);
 });
 
-//test to add an empty string
-test('Sum of an empty string is 0', () => {
+//Test 2: test to add an empty string
+test('To Test the sum of an empty string to be 0', () => {
     expect(stringCalc('')).toBe(0);
 });
 
-//test to add numbers separated by the new line characters
-// test('Sum of numbers separated by new line characters is 5', () => {
-//     expect(util.stringCalc(2 \n 3)).toBe(5);
-// });
+//Test 3: test to add numbers separated by the new line characters
+test('To test the sum of two numbers separated by new line characters', () => {
+    expect(stringCalc('2\n3')).toBe(5);
+});
 
-//test to add multiple numbers
-test('Sum of multiple numbers separated by commas like 1,2,3 is 6', () => {
-    expect(stringCalc(1,2,3)).toBe(6);
+//Test 4: test for negative numbers 
+test('To Test the sum when the input is a negative number', () => {
+    expect(function(){stringCalc('-1')}).toThrow('negative numbers are not allowed');
+});
+
+//Test 5: test to add multiple numbers
+test('To Test the sum when more than two numbers are added', () => {
+    expect(stringCalc('1,2,3')).toBe(6);
+});
+
+//Test 6: test to add numbers separated by multiple delimiters
+test('To Test the sum of numbers separated by multiple delimiters', () => {
+    expect(stringCalc('//;\n1;2')).toBe(3);
+});
+
+//Test 7: test for all negative number inputs
+test('To Test the sum when all the inputs are negative numbers', () => {
+    expect(function(){stringCalc('-1,-2')}).toThrow('negative numbers are not allowed');
+});
+
+//Test 7: test for both positive and negative numbers 
+test('To Test the sum when the inputs are both positive and negative numbers', () => {
+    expect(function(){stringCalc('-1,-2,3;4')}).toThrow('negative numbers are not allowed');
 });
